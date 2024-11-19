c0e9723c6cbecb1c7478a818003fcc537ecbe114
hazzuk <git@hazz.uk>
2024-11-19 00:03:31 +0000

    feat: create worker.js

---
+++ scripts/worker.js
@@ -0,0 +1,61 @@
+// Copyright (c) 2024 hazzuk
+// SPDX-License-Identifier: Apache-2.0
+
+// [background script]
+
+// context menu
+{
+    const menuPattern = {
+        flightradar: "*://www.flightradar24.com/data/flights/*",
+        simbrief: "*://dispatch.simbrief.com/options/new"
+    };
+
+    // menu structure
+    {
+        // remove all existing context menus
+        chrome.contextMenus.removeAll();
+
+        // flightdata
+        chrome.contextMenus.create({
+            id: "menu-flightdata",
+            title: "flightdata",
+            contexts: ["page"],
+            documentUrlPatterns: [
+                menuPattern.flightradar,
+                menuPattern.simbrief
+            ]
+        });
+
+        // submenu
+        {
+            // flightradar
+            chrome.contextMenus.create({
+                id: "menu-flightradar",
+                parentId: "menu-flightdata",
+                title: "Copy flight data",
+                contexts: ["page"],
+                documentUrlPatterns: [menuPattern.flightradar]
+            });
+
+            // simbrief
+            chrome.contextMenus.create({
+                id: "menu-simbrief",
+                parentId: "menu-flightdata",
+                title: "Insert flight data",
+                contexts: ["page"],
+                documentUrlPatterns: [menuPattern.simbrief]
+            });
+        }
+    };
+
+    // menu event listeners
+    chrome.contextMenus.onClicked.addListener((info, tab) => {
+        // send message to content scripts
+        if (info.menuItemId === "menu-flightradar") {
+            chrome.tabs.sendMessage(tab.id, { action: "run_getFlightData" });
+        } else if (info.menuItemId === "menu-simbrief") {
+            chrome.tabs.sendMessage(tab.id, { action: "run_insertFlightData" });
+        }
+    });
+
+};
